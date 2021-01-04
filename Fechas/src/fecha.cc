f0c25073b5f81e09b0c365923d4c6893c38b215d
Ivan-Garcia-02 <alu0101388786@ull.edu.es>
2021-01-04 14:18:34 +0000

    Empezado y creado Cmake.txt

---
+++ Fechas/src/fecha.cc
@@ -0,0 +1,18 @@
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Informática Básica
+  *
+  * @file fecha.cc (definiciones)
+  * @author Iván García González alu0101388786@ull.edu.es
+  * @date 7 Enero 2021
+  * @brief Desarrolle una clase Fecha que permita representar y gestionar fechas. Incorpore en la clase los miembros de datos 
+  *        y métodos que considere adecuados para la finalidad que se persigue en este ejercicio. 
+  *        Incluya un método que permita determinar si el año correspondiente a una fecha es un año bisiesto o no. 
+  *        Realice un programa cliente fechas.cc que tome como parámetro una fecha, un número y un nombre de fichero.
+  *        El programa deberá imprimir en el fichero de salida (tercer parámetro) las N (segundo parámetro) fechas cronológicamente 
+  *        posteriores a la introducida (primer parámetro) con una separación de un día entre fechas sucesivas.
+  * @bug No hay bugs conocidos
+  * @see https://github.com/ULL-ESIT-IB-2020-2021/IB-P11-Classes-CMake/blob/master/Classes-CMake.md
+  */
